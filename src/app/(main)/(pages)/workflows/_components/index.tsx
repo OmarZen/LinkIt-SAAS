4742fcb860d3d937de263af32ed9b9bde2bf5d4f
OmarZen <omarwaleedzenhom2002@gmail.com>
2024-05-30 17:02:18 +0300

    Integration with Discord - Slack - Notion - Google Drive

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import Workflow from './workflow'
+import { onGetWorkflows } from '../_actions/workflow-connections'
+// import MoreCredits from './more-creadits'
 
 type Props = {}
 
-const Workflows = (props: Props) => {
+const Workflows = async (props: Props) => {
+  const workflows = await onGetWorkflows()
   return (
     <div className="relative flex flex-col gap-4">
-        <section className="flex flex-col m-2">
-            <Workflow 
-            description='Creating a test workflow' 
-            id='sdfvsfe' 
-            name='Automation Workflow' 
-            publish={false}
+      <section className="flex flex-col m-2">
+        {/* <MoreCredits /> */}
+        {workflows?.length ? (
+          workflows.map((flow) => (
+            <Workflow
+              key={flow.id}
+              {...flow}
             />
-        </section>
+          ))
+        ) : (
+          <div className="mt-28 flex text-muted-foreground items-center justify-center">
+            No Workflows
+          </div>
+        )}
+      </section>
     </div>
   )
 }
